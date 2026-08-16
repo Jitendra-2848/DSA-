84f63bd238b98b163dbd888c97cef8d0fcac79a7
Jitendra Prajapati <prajapatijitendra2848@gmail.com>
2026-08-16 23:36:51 +0530

    Time: 2 ms (99.96%), Space: 52.6 MB (41.15%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,15 +1,46 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> arr = new ArrayList<>();
+        int count1 = 0;
+        int count2 = 0;
+        int candidate1 = 0;
+        int candidate2 = 0;
+
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > (nums.length/3)) {
-                arr.add(entry.getKey());
+            if (candidate1 == num) {
+                count1++;
+            } else if (candidate2 == num) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
         }
-        return arr;
+        List<Integer> result = new ArrayList<>();
+        int target = nums.length / 3;
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (candidate1 == num) {
+                count1++;
+            }
+            if (candidate2 == num) {
+                count2++;
+            }
+        }
+
+        if (count1 > target) {
+            result.add(candidate1);
+        }
+        if (count2 > target && candidate2 != candidate1) {
+            result.add(candidate2);
+        }
+
+        return result;
     }
 }
